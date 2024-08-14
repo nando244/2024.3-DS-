f573f283e7eb704d90d7a7a949226427e354bf4e
nando244 <134401180+nando244@users.noreply.github.com>
2024-08-14 09:28:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,112 @@
-const caixaPrincipal = Document.querySelector('. caixa-principal');
-const caixaPerguntas = document
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Enunciado 01",
+        alternativas: [
+            {
+                texto:"alternativa 01",
+                afirmacao: "resultado 01"
+            },
+            {
+                texto:"alternativa 02",
+                afirmacao: "resultado 02"
+            }   
+        ]
+    },
+    {
+        enunciado: "Enunciado 02",
+        alternativas: [
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado03"
+            },
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
+            }
+        ]
+    },
+    {
+        enunciado: "Enunciado 03",
+        alternativas: [
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+            },
+            {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+            }
+        ]
+    },
+    {
+        enunciado: "Enunciado 04",
+        alternativas: [
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
+        ]
+    },
+    {
+        enunciado: "Enunciado 05",
+        alternativas: [
+            {
+                texto: "alternativa 09",
+                afirmacao: "resultado 09"
+            },
+            {
+                texto: "alternativa 10",
+                afirmacao: "resultado 10"
+            }
+        ]
+    },
+]
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostrarPerguntas(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = ""
+    mostrarAlternativas();
+}
+
+function mostrarAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostrarPerguntas();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = " Inicio do texto... "
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostrarPerguntas();
